5ef1edf990693fbde229d04bb6ab1e7edc7fd11c
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-09-25 20:13:06 +0300

    Task4.V22

---
+++ Tyuiu.YushkovaES.Sprint1.Task4.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.YushkovaES.Sprint1.Task4.V22.Lib;
+namespace Tyuiu.YushkovaES.Sprint1.Task4.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 5;
+            double wait = 0.023;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res, 0.001);
+        }
+    }
+}
---
+++ Tyuiu.YushkovaES.Sprint1.Task4.V22/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.YushkovaES.Sprint1.Task4.V22.Lib;
+
+namespace Tyuiu.YushkovaES.Sprint1.Task0.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Екатерина Е. С | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Class Math                                                       *");
+            Console.WriteLine("* Задание #4                                                             *");
+            Console.WriteLine("* Вариант #22                                                            *");
+            Console.WriteLine("* Выполнил: Екатерина Е. С | ИСПб-25-1                                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные *");
+            Console.WriteLine("* вычисляет результаты по формуле и печатает его на экране               *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int x, y;
+            Console.Write("Введите значение x: ");
+
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Ответ выражения = " + ds.Calculate(x, y));
+            Console.ReadKey();
+
+
+        }
+    }
+}
