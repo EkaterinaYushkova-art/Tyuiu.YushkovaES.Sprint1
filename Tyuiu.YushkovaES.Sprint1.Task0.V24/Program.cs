fee84bd7516576df7872e2e9a0a19fd48a1c7373
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-09-25 15:11:19 +0300

    Task0.V24

--- Tyuiu.YushkovaES.Sprint1.Task0.V24/Program.cs
+++ Tyuiu.YushkovaES.Sprint1.Task0.V24/Program.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.YushkovaES.Sprint1.Task0.V24
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                       *");
             Console.WriteLine("* Задание #0                                                             *");
             Console.WriteLine("* Вариант #24                                                            *");
-            Console.WriteLine("* Выполнил: Слоква Г. А. | ИСПб-25-1                                     *");
+            Console.WriteLine("* Выполнил: Екатерина Е. С | ИСПб-25-1                                   *");
             Console.WriteLine("**************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                               *");
             Console.WriteLine("* Написать программу, которая вычисляет выражение 2 * 4 / 4 / 2 + 1      *");
