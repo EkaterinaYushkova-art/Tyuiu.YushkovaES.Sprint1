b900f4a957c50cf89583d932e1fe9eb58e6aa02e
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-09-26 17:28:19 +0300

    Task5.V3

---
+++ Tyuiu.YushkovaES.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.YushkovaES.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.YushkovaES.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 12789;
+            int wait = 7;
+            int res = ds.Calculate(x);
+            
+            Assert.AreEqual(wait, res); 
+        }
+    }
+}
---
+++ Tyuiu.YushkovaES.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.YushkovaES.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.YushkovaES.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Екатерина Е. С | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                       *");
+            Console.WriteLine("* Задание #5                                                             *");
+            Console.WriteLine("* Вариант #3                                                             *");
+            Console.WriteLine("* Выполнил: Екатерина Е. С | ИСПб-25-1                                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Присвоить целой переменной h третью от конца цифру                     *");
+            Console.WriteLine("* в записи положительного целого числа k                                 *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+
+            Console.Write("Введите положительное число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            int res = Convert.ToInt32(ds.Calculate(x));
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
